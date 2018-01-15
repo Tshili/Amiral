f66fc8317a04e39a73d4ea219a3304bf16d0adb6
Bernard MPOY <bernard.mpoy@corp.talan.com>
2018-01-15 10:52:14 +0100

    IHM satellite vessel

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FlagshipShipComponent } from './flagship-ship/flagship-ship.component';
 import { NavComponent } from './nav/nav.component';
+import { InformationComponent } from './information/information.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FlagshipShipComponent,
-    NavComponent
+    NavComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule
